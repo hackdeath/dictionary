4ca9fe5a732dfeca4c332a1dba202d0653435f17
tkovs <victor.rdg@hotmail.com>
2016-07-05 15:42:45 -0300

    Banco de dados finalizado

--- dictionary/models.py
+++ dictionary/models.py
@@ -1,5 +1,29 @@
 from django.db import models
 
 class Word(models.Model):
-    term = models.CharField(max_length=30)
-    meaning = model.CharField(max_length=100)
+    id_word    = models.IntegerField()
+    term       = models.CharField(max_length=30)
+    definition = models.CharField(max_length=50)
+    language   = models.CharField(max_length=5)
+    category   = models.CharField(max_length=30)
+    
+"""
+####BD Idea
+
+Word
+    id_word
+    term
+    definition
+    language
+    category
+
+####Sample
+
+Word
+
+  | id | id_word | term    | definition | language | category |
+  | -- | ------- | ----    | ---------- | -------- | -------- |
+  | 01 | 01      | carro   | ...        | pt_br    | ...      | 
+  | 02 | 01      | carrito | ...        | es_es    | ...      |
+
+"""
