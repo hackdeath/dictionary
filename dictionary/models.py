f142017b15dd09c8f0aeabbb6650589ec1341b57
tkovs <victor.rdg@hotmail.com>
2016-07-07 16:48:34 -0300

    Alteração no banco de dados
    Adaptação necessária para suportar diferentes linguagens que implica em diferentes alfabetos.

--- dictionary/models.py
+++ dictionary/models.py
@@ -2,28 +2,35 @@ from django.db import models
 
 class Word(models.Model):
     id_word    = models.IntegerField()
+    language   = models.ForeignKey(Language, on_delete=models.CASCADE)
     term       = models.CharField(max_length=30)
     definition = models.CharField(max_length=50)
-    language   = models.CharField(max_length=5)
     category   = models.CharField(max_length=30)
+
+    def __str__(self):
+        return "({0} - {1}) {2}: {3}".format(id_word, language, term, definition)
+
+class Language(models.Model):
+    language = models.CharField(max_length=5, primary_key=True)
+    alphabet = models.CharField(max_length=50)
+
+    def __str__(self):
+        return "{0}: {1}".format(language, alphabet)
     
 """
-####BD Idea
-
-Word
-    id_word
-    term
-    definition
-    language
-    category
-
 ####Sample
 
 Word
+  | id | id_word | language | id_word | term    | definition | category |
+  | -- | ------- | -------- | ------- | ----    | ---------- | -------- |
+  | 01 | 01      | pt_br    | 01      | olá     | ...        | ...      | 
+  | 02 | 01      | es_es    | 01      | hello   | ...        | ...      |
 
-  | id | id_word | term    | definition | language | category |
-  | -- | ------- | ----    | ---------- | -------- | -------- |
-  | 01 | 01      | carro   | ...        | pt_br    | ...      | 
-  | 02 | 01      | carrito | ...        | es_es    | ...      |
+Language
 
+  | language |           alphabet          |
+  | -------- | --------                    |
+  | pt-br    | abcdefghijklmnopqrstuvwxyz  |
+  | en-us    | abcdefghijklmnopqrstuvwxyz  |
+  | es-es    | abcdefghijklmnñopqrstuvwxyz |
 """
