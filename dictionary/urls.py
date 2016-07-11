2e77ebc9c9d43f8f9da9318cb6a53d9252def981
AntonioVitor <antoniovitorFBN@gmail.com>
2016-07-11 14:19:01 -0300

    Corrigindo bug na URL ^dictionary/(word). Adicionando método index para teste de URL's

--- dictionary/urls.py
+++ dictionary/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-	url(r'^$', views.detailWord, name='index'),
-	url(r'^(?P<word>[a-z]+)/$', views.detailWord, name='detailWord')
+	url(r'^$', views.index, name='index'),
+	url(r'^(?P<word>[A-Za-z]+)/', views.detailWord, name='detailWord')
 ]
--- dictionary/views.py
+++ dictionary/views.py
@@ -3,5 +3,8 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 
-def detailWord(request):
+def detailWord(request, word):
 	return HttpResponse("This is the page of word's details")
+
+def index(request):
+	return HttpResponse("index")
