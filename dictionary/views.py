116cd65da355e1d878d682071580a7545b93304b
tkovs <victor.rdg@hotmail.com>
2016-07-11 16:36:44 -0300

    Desenvolvimento inicial de view e template para palavra

--- dictionary/views.py
+++ dictionary/views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import render
-
-# Create your views here.
 from django.http import HttpResponse
+from django.template import loader
+from .models import Word, Language
 
 def detailWord(request, word):
-    return HttpResponse("This is the page of {0}'s details".format(word))
+    ids = [obj.id for obj in Word.objects.filter(term=word)]
+    words = Word.objects.filter(id_word__in=ids)
+
+    template = loader.get_template('detailWord.html')
+    context = {
+            'words': words,
+    }
+
+    return HttpResponse(template.render(context, request))
 
 def index(request):
     return HttpResponse("index")
