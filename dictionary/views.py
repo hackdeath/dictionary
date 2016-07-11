32dd58ea94ed82cfd187b21ba8e0d6ed696ff451
fsdude <arthursavioeugenio@hotmail.com>
2016-07-11 16:43:40 -0300

    Listando os alfabetos

--- dictionary/views.py
+++ dictionary/views.py
@@ -2,9 +2,12 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
+from .models import Language, Word
 
 def detailWord(request, word):
 	return HttpResponse("This is the page of word's details")
 
 def index(request):
-	return HttpResponse("index")
+    alphabet_list = Language.objects.order_by('language')
+    context = {'alphabet_list': alphabet_list}
+    return render(request, 'index.html', context)
