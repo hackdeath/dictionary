90f048a3a6835ceb707688917bda64fbfada059f
tkovs <victor.rdg@hotmail.com>
2016-07-13 14:18:42 -0300

    Uso de espaços em url de palavra

--- dictionary/urls.py
+++ dictionary/urls.py
@@ -5,5 +5,5 @@ from . import views
 app_name = 'dictionary'
 urlpatterns = [
 	url(r'^$', views.index, name='index'),
-	url(r'^word/(?P<word>[A-Za-z]+)/', views.detailWord, name='detailWord')
+	url(r'^word/(?P<word>[A-Za-z\W]+)/', views.detailWord, name='detailWord')
 ]
