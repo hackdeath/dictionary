f04a8b4bcf91e8cbed1a0a2d44908e6bb23f9d76
fsdude <arthursavioeugenio@hotmail.com>
2016-07-18 16:46:40 -0300

    regex de letter (lang section) simplificada

--- dictionary/urls.py
+++ dictionary/urls.py
@@ -5,6 +5,6 @@ from . import views
 app_name = 'dictionary'
 urlpatterns = [
 	url(r'^$', views.index, name='index'),
-	url(r'^letter/(?P<lang>.*)/(?P<letter>.{1})/', views.letter, name='letter'),
+	url(r'^letter/(?P<lang>[A-Za-z-]+)/(?P<letter>.)/', views.letter, name='letter'),
 	url(r'^word/(?P<word>[A-Za-z\W]+)/', views.detailWord, name='detailWord')
 ]
