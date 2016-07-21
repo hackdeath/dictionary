40a368ca31ec658caa340032ff39396ce4fcc13b
tkovs <victor.rdg@hotmail.com>
2016-07-21 15:33:53 -0300

    Url de palavras com acentos em detailsView corrigida

--- dictionary/urls.py
+++ dictionary/urls.py
@@ -6,5 +6,5 @@ app_name = 'dictionary'
 urlpatterns = [
 	url(r'^$', views.index, name='index'),
 	url(r'^letter/(?P<lang>[A-Za-z-]+)/(?P<letter>.)/', views.letter, name='letter'),
-	url(r'^word/(?P<word>[A-Za-z\W]+)/', views.detailWord, name='detailWord')
+	url(r'^word/(?P<word>.+)/', views.detailWord, name='detailWord')
 ]
