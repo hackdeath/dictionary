5ad14e9eab582268ba090b2f0ebae4e65fc287bc
tkovs <victor.rdg@hotmail.com>
2016-08-03 15:38:49 -0300

    Esconde id de palavra do usuário

--- dictionary/forms.py
+++ dictionary/forms.py
@@ -2,7 +2,7 @@ from  django import forms
 from .models import Language, Word
 
 class WordForm(forms.Form):
-    id_word    = forms.CharField(Word.objects.all().last().id_word + 1, widget=forms.TextInput(attrs={'readonly':'readonly'}))
+    id_word    = forms.CharField(Word.objects.all().last().id_word + 1, widget=forms.HiddenInput)
     language   = forms.ModelChoiceField(queryset = Language.objects.all())
     term       = forms.CharField(max_length=30)
     definition = forms.CharField(max_length=150)
